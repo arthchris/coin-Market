0b421bcc656030a76641f7458f8f3ca2d7963caa
christian <arthurchristian111@gmail.com>
2023-02-01 23:15:01 +0000

    coin

--- src/app/services/coinMarketApiService.ts
+++ src/app/services/coinMarketApiService.ts
@@ -7,7 +7,7 @@ export class coinMarketApiServices {
 
   coinMarketApi() {
     return this.http.get<any>(
-      'https://pro-9b47b5a3-1bcc-4bd1-9e57-9e9820b93ed1.coinmarketcap.com'
+      'https://pro-9b47b5a3-1bcc-4bd1-9e57-9e9820b93ed1.coinmarketcap'
     );
   }
 }
